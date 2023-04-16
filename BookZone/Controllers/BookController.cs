38f33c3964c20a8f21fad1ae6c53068417a726ee
Shakhawat Hossain <shakhawathossain.se@gmail.com>
2023-04-17 01:26:10 +0600

    added the book list page

---
+++ BookZone/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using BookZone.Data;
+using BookZone.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookZone.Controllers
+{
+	public class BookController : Controller
+	{
+
+		private readonly AppDbContext _db;
+
+		public BookController(AppDbContext db)
+		{
+			_db = db;
+
+		}
+		public IActionResult Index()
+		{
+			IEnumerable<Book> bookList = _db.Books;
+
+
+			return View(bookList);
+		}
+	}
+}

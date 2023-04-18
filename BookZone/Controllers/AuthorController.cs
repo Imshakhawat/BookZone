36afeb84f26c81f5d6f899a2f6924c3e25ef51e7
Shakhawat Hossain <shakhawathossain.se@gmail.com>
2023-04-18 14:36:07 +0600

    implemented delete operation on books

--- BookZone/Controllers/AuthorController.cs
+++ BookZone/Controllers/AuthorController.cs
@@ -29,5 +29,45 @@ namespace BookZone.Controllers
 
             return View(authorWithBooks);
         }
+
+
+
+        public IActionResult Edit(int Id)
+        {
+
+            //var author = _db.Authors.Find(Id);
+            // var author = _db.Authors.FirstOrDefault(u => u.Id == Id);
+
+            var selectedBook = _db.Books.Include(a => a.Author).FirstOrDefault(a => a.Id == Id);
+
+            return View(selectedBook);
+        }
+
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Book obj)
+        {
+
+
+            var selectedBook = _db.Books.Include(a => a.Author).FirstOrDefault(a => a.Id == obj.Id);
+
+            selectedBook.Title = obj.Title;
+
+
+            // _db.Books.Update(obj);
+            //_db.Add(book);
+            _db.SaveChanges();
+            //  TempData["success"] = "Category updated successfully";
+            // return RedirectToAction("Index");
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
     }
 }
--- BookZone/Controllers/BookController.cs
+++ BookZone/Controllers/BookController.cs
@@ -22,5 +22,70 @@ namespace BookZone.Controllers
 
 			return View(bookList);
 		}
-	}
+
+        public IActionResult Details(int Id)
+        {
+
+            //var author = _db.Authors.Find(Id);
+            // var author = _db.Authors.FirstOrDefault(u => u.Id == Id);
+
+            var selectedBook = _db.Books.Include(a => a.Author).FirstOrDefault(a => a.Id == Id);
+
+            return View(selectedBook);
+        }
+
+        public IActionResult Edit(int Id)
+        {
+
+            //var author = _db.Authors.Find(Id);
+            // var author = _db.Authors.FirstOrDefault(u => u.Id == Id);
+
+            var selectedBook = _db.Books.Include(a => a.Author).FirstOrDefault(a => a.Id == Id);
+
+            return View(selectedBook);
+        }
+
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Book obj)
+        {
+
+
+            var selectedBook = _db.Books.Include(a => a.Author).FirstOrDefault(a => a.Id == obj.Id);
+
+            selectedBook.Title = obj.Title;
+
+
+           // _db.Books.Update(obj);
+            //_db.Add(book);
+            _db.SaveChanges();
+            //  TempData["success"] = "Category updated successfully";
+            // return RedirectToAction("Index");
+
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+
+            var selectedBook = _db.Books.Include(a => a.Author).FirstOrDefault(a => a.Id == id);
+
+            _db.Remove(selectedBook);
+
+            _db.SaveChanges();
+
+
+            return RedirectToAction("Index"); ;
+        }
+
+
+
+
+
+
+
+    }
 }
